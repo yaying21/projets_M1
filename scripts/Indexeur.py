650f4ee982c72f4b3b4b2869e85302c9d9a53736
lpschaub <lp.schaub@gmail.com>
2020-02-09 00:08:50 +0100

    changements

--- scripts/Indexeur.py
+++ scripts/Indexeur.py
@@ -1,6 +1,9 @@
 
 import nltk
+import glob
+import pprint
 from Evaluation import Evaluation
+
 class Indexeur(object):
 
     def __init__(self,tool):
@@ -16,17 +19,17 @@ class Indexeur(object):
 
 
 if __name__ == '__main__':
-    ind = Indexeur(nltk)
-    text = "c'est vraiment pas mauvais hein"
-    text2 = "franchement c'est éclaté"
-    text3 = "ouais je suis d'accord la sauce est vraiment mauvaise"
-    pred = []
-    ref = ['1','1','0']
-    pred.append(ind.requetage(text,requete = " 'mauvais' in text"))
-    pred.append(ind.requetage(text2,requete=" 'mauvais' in text"))
-    pred.append(ind.requetage(text3,requete=" 'mauvais' in text"))
-    print(pred)
-    eval = Evaluation(ref,pred)
-    print(eval.f_mesure())
+    # ind = Indexeur(nltk)
+    # text = "c'est vraiment pas mauvais hein"
+    # text2 = "franchement c'est éclaté"
+    # text3 = "ouais je suis d'accord la sauce est vraiment mauvaise"
+    # pred = []
+    # ref = ['1','1','0']
+    # pred.append(ind.requetage(text,requete = " 'mauvais' in text"))
+    # pred.append(ind.requetage(text2,requete=" 'mauvais' in text"))
+    # pred.append(ind.requetage(text3,requete=" 'mauvais' in text"))
+    # print(pred)
+    # eval = Evaluation(ref,pred)
+    # print(eval.f_mesure())
 
 
