afdce86dab261c5006f747040a842f64d836518e
Solveig <solveig@LAPTOP-49D31SSK.localdomain>
2020-02-09 00:24:33 +0100

    ajout du script Solveig

---
+++ scripts/indexInverse_solveig.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from collections import Counter, defaultdict
+
+corpus = defaultdict()
+corpus["doc1"] = "le repas est pas mauvais"
+corpus["doc2"] = "si, je le trouve pas bon"
+corpus["doc3"] = "j’avoue il est vraiment mauvais"
+
+dico = defaultdict(list)
+tokens = defaultdict()
+for cle, valeur in corpus.items():
+    tokens[cle] = valeur.split(" ")
+cnt = defaultdict(Counter)
+for cle, valeur in tokens.items():
+    for item in valeur:
+        cnt[cle][item] += 1
+        dico[item].append([cle, cnt[cle][item]])
+        
+        
+for k in sorted(dico.keys()):
+    print("%s: %s" % (k, dico[k]))
+        
+os.system("pause")
+
